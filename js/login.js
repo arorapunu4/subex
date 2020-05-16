2ff66078db8b6eee404745f6dcc803175df45993
Balaji Venkatraman <balaji@moonraft.com>
2020-05-17 00:35:27 +0530

    added password auth

--- js/login.js
+++ js/login.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
   var totalPossibleAttempts = 3;
+  var password = '1234567';
   var validMail = false;
   var validPassword = false;
+
+  // reset defaults
+  var form = document.forms[0];
+  for (var i = 0; i < form.length; i++) {
+    if (form[i].tagName === 'INPUT') {
+      form[i].value = '';
+    }
+  }
 
   function validateEmail(email) {
     var emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -32,6 +41,16 @@ $(document).ready(function() {
     errorEle.classList.add('in-visible');
   }
 
+  function checkValidity() {
+    if (validMail && validPassword) {
+      document
+        .querySelector('button[type="submit"]')
+        .classList.remove('disabled');
+    } else {
+      document.querySelector('button[type="submit"]').classList.add('disabled');
+    }
+  }
+
   $('#email-input').focusout(function(e) {
     var email = this.value;
     var checkElem = document.querySelector('.check');
@@ -48,6 +67,7 @@ $(document).ready(function() {
       checkElem.classList.remove('show');
       checkElem.classList.add('hide');
     }
+    checkValidity();
   });
 
   $('#password-input').focusout(function(e) {
@@ -61,14 +81,31 @@ $(document).ready(function() {
       removeError(e, this);
       hideErrorMessage(this);
     }
+    checkValidity();
   });
 
-  $('#password-input ~ img').click(function(e) {
+  $('#password-input ~ img').click(function() {
     var passwordElem = this.parentNode.firstElementChild;
     if ($(passwordElem).attr('type') === 'password') {
-      $(passwordElem).attr('type','text');
+      $(passwordElem).attr('type', 'text');
     } else {
       $(passwordElem).attr('type', 'password');
+    }
+  });
+
+  $('.signin').click(function(e) {
+    e.preventDefault();
+    var passwordInput = document.querySelector('#password-input');
+    if (totalPossibleAttempts <= 1) {
+      window.location.href = '/index.html';
+      return;
+    }
+    if (passwordInput.value !== password) {
+      totalPossibleAttempts--;
+      document.querySelector('.error-container').classList.add('visible');
+      document.querySelector('.count').innerHTML = totalPossibleAttempts;
+    } else {
+      document.querySelector('.error-container').classList.remove('visible');
     }
   });
 });
